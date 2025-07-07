87e93a06423ff6aadd72216197d122dc6a0f80b5
Dania Mohd <44768559+DaniaMohd@users.noreply.github.com>
2025-07-07 17:25:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Selenium_In_C
+ 
---
+++ Selenium_In_C#/Program.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // start chrome browser
+        IWebDriver driver = new ChromeDriver();
+
+        //open a website
+        driver.Navigate().GoToUrl("https://www.google.com");
+
+        //find the search box
+        IWebElement searchbox = driver.FindElement(By.Name("q"));
+
+        //type in the search box
+        searchbox.SendKeys("Selenium C# example");
+
+        //submit input
+        searchbox.Submit();
+
+        //wait for a few seconds for results to load
+        System.Threading.Thread.Sleep(3000);
+
+        //close the browser
+        driver.Quit();
+    }
+}
